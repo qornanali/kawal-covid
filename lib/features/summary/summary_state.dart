ecb257c6406cde79a29ace75584d8da27108ca7f
Ali Qornan <aliqornan97@gmail.com>
2021-07-09 20:25:16 +0700

    [Ali] Add article viewer

--- lib/features/summary/summary_state.dart
+++ lib/features/summary/summary_state.dart
@@ -4,6 +4,7 @@ import 'package:kawal_covid/api/kawal_covid/kawal_corona_client.dart';
 import 'package:kawal_covid/api/kawal_covid/kawal_corona_client_impl.dart';
 import 'package:kawal_covid/api/kawal_covid/model/covid_summary.dart';
 import 'package:kawal_covid/core/entity/api_response.dart';
+import 'package:kawal_covid/features/articleviewer/article_viewer_page.dart';
 import 'package:kawal_covid/features/statisticlist/statistic_list_page.dart';
 import 'package:kawal_covid/features/summary/summary_page.dart';
 
@@ -34,45 +35,72 @@ class SummaryState extends State<SummaryPage> {
     return Container(
         padding: const EdgeInsets.only(top: 20.0, bottom: 10.0, left: 10.0, right: 10.0),
         child: Column(
-          children: [_buildImageVirus(buildContext), _buildIndonesiaStatisticPanel(buildContext)],
+          children: [
+            _buildImageVirus(buildContext),
+            _buildIndonesiaSummaryPanel(buildContext),
+            _buildButton('Statistik provinsi di Indonesia', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => StatisticListPage()),
+              );
+            }),
+            _buildButton("Gejala-gejala yang timbul", () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ArticleViewerPage(
+                      pageTitle: "Gejala COVID19",
+                      articleTitle: "Kenali Gejala Orang Terinfeksi Virus Corona di Minggu Pertama",
+                      articleContentFile: "assets/texts/articles-gejala-covid.txt",
+                    )),
+              );
+            }),
+            _buildButton("Protokol 5M", () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ArticleViewerPage(
+                          pageTitle: "Protokol 5M",
+                          articleTitle: "Mengenal Protokol Kesehatan 5M untuk Cegah COVID-19",
+                          articleContentFile: "assets/texts/articles-5-m.txt",
+                        )),
+              );
+            }),
+          ],
         ));
   }
 
-  Widget _buildIndonesiaStatisticPanel(BuildContext buildContext) {
-    return Center(
-      child: FutureBuilder<ApiResponse<CovidSummary>>(
-        future: _covidSummary,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var data = snapshot.data.data;
-            return Align(
-                child: Column(
-              children: [
-                Text("Jumlah positif: ${data.positiveCount}", textAlign: TextAlign.left),
-                Text("Jumlah sembuh: ${data.recoveredCount}", textAlign: TextAlign.left),
-                Text("Jumlah meninggal: ${data.deathCount}", textAlign: TextAlign.left),
-                Text("Jumlah yang sudah dirawat: ${data.treatedCount}", textAlign: TextAlign.left),
-                ElevatedButton(
-                  child: Text('Statistik provinsi di Indonesia'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => StatisticListPage()),
-                    );
-                  },
-                )
-              ],
-            ));
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
-          return CircularProgressIndicator();
-        },
-      ),
+  Widget _buildIndonesiaSummaryPanel(BuildContext buildContext) {
+    return FutureBuilder<ApiResponse<CovidSummary>>(
+      future: _covidSummary,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          var data = snapshot.data.data;
+          return Align(
+              child: Column(
+            children: [
+              Text("Jumlah positif: ${data.positiveCount}", textAlign: TextAlign.left),
+              Text("Jumlah sembuh: ${data.recoveredCount}", textAlign: TextAlign.left),
+              Text("Jumlah meninggal: ${data.deathCount}", textAlign: TextAlign.left),
+              Text("Jumlah yang sudah dirawat: ${data.treatedCount}", textAlign: TextAlign.left),
+            ],
+          ));
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        return CircularProgressIndicator();
+      },
     );
   }
 
   Widget _buildImageVirus(BuildContext buildContext) {
     return Image(image: AssetImage('assets/images/virus.png'));
+  }
+
+  Widget _buildButton(String text, VoidCallback onPressed) {
+    return ElevatedButton(
+      child: Text(text),
+      onPressed: onPressed,
+    );
   }
 }
